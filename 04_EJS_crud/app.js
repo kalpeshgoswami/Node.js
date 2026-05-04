37c44b9b8daf19897fb5e79f23fed5ec753e65b0
kalpeshgoswami <kalpeshgoswami298@gmail.com>
2026-05-04 12:26:15 +0530

    add edit

--- 04_EJS_crud/app.js
+++ 04_EJS_crud/app.js
@@ -4,7 +4,7 @@ const app = express();
 
 app.set("view engine", "ejs");
 
-app.use(express.urlencoded({extended: true }));    
+app.use(express.urlencoded({ extended: true }));
 
 let studentList = [{
     id: 1,
@@ -23,7 +23,7 @@ app.get("/add", (req, res) => {
     res.render("add")
 })
 
-app.post("/add", (req, res)=>{
+app.post("/add", (req, res) => {
 
     const { name } = req.body;
 
@@ -37,6 +37,48 @@ app.post("/add", (req, res)=>{
     res.redirect("/");
 
 });
+
+app.get("/edit/:id", (req, res) => {
+
+    const id = req.params.id;
+
+    console.log("id", id);
+
+    const student = studentList.find(s => s.id === Number(id));
+
+    if (!student) {
+        return res.send("student not found");
+    }
+
+    res.render("edit", { student });
+});
+
+app.post("/edit/:id", (req, res) => {
+    const id = req.params.id;
+
+    const student = studentList.find(s => s.id === Number(id));
+
+    if (!student) {
+        return res.send("student not found");
+    }
+
+    student.name = req.body.name;
+
+    res.redirect("/");
+});
+
+
+app.get("/delete/:id", (req, res) => {
+    const id = req.params.id
+
+    console.log("id", id)
+
+    studentList = studentList.filter(s => s.id !== Number(id));
+
+    res.redirect("/");
+});
+
+
 
 const port = 5000;
 
